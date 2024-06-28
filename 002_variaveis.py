02f51168720553d86681009d0318fd133456cfc2
Isra <israelklink@gmail.com>
2024-06-27 23:46:29 -0300

    variaveis em python

---
+++ 002_variaveis.py
@@ -0,0 +1,38 @@
+"""
+Programação estruturadas
+
+"""
+"""
+Variáveis - uma variável nada mais é que a representação de um valor;
+Variáveis são rótulos e identificam objetos.
+
+variáveis devem ser iniciadas com letras,
+vairáveis não podem ter espaços,
+variáveis não podem começar com números,
+
+exemplos de variáveis:
+_media
+media
+
+Em Python não precisamos declarar o timpo de variável, 
+Em Python todas variáveis são objetos. 
+
+exemplo:
+
+idade = 10 ( o lado direito é executado primeiro, ou seja, o objeto 10 é criado, depois a variável idade é 
+            atribuida ao objeto 10)
+
+"""
+
+idade = 10 
+print(idade)
+
+pi=3.1415 
+print(type(pi)) # float
+
+# variáveis tipo lógicas - True or False
+
+aprovado=True
+print(aprovado)
+resultado=False
+print(resultado)
