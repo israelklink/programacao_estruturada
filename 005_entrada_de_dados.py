79561c11263ede6fb0aa02905958122c17f83dfe
Isra <israelklink@gmail.com>
2024-06-28 12:58:17 -0300

    entrada de dados

---
+++ 005_entrada_de_dados.py
@@ -0,0 +1,20 @@
+"""
+Programação Estruturada
+
+"""
+
+"""
+Entrada de Dados 
+
+"""
+
+idade = input('digite sua idade:')
+print(idade)
+
+#para receber somente números inteiros
+
+idade_=int(input("digit sua idade (numero interio):"))
+print(idade_)
+
+var_pi=float(input("entre com o valor de pi:"))
+print("valor de Pi eh:", var_pi)
