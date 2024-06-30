9740e2b42ba6a0d9112bfc344c810f3dfc5581c1
Isra <israelklink@gmail.com>
2024-06-30 20:21:30 -0300

    modulos

---
+++ 013_modulos.py
@@ -0,0 +1,16 @@
+"""
+Programação Estruturada
+
+"""
+
+"""
+
+Módulos - é um arquivo em python .py
+Todo aquivo .py é um módulo.
+
+Eu utilizo muito pouco o módulo. Basicamente é:
+
+import (função).py
+
+
+"""
